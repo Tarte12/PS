e8b693275c4a6d43da5199f30facbb86306a64dc
KimHuiSu <101166683+Tarte12@users.noreply.github.com>
2026-07-10 08:22:43 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,10 +1,11 @@
 import math
+
 def solution(numer1, denom1, numer2, denom2):
-    numer = numer1*denom2 + numer2*denom1
-    denom = denom1*denom2
-    
-    #최대공약수 내장 함수 gcd
-    gcd = math.gcd(numer, denom)
-    answer = [numer // gcd, denom // gcd]
+    answer = []
+    answer.append(numer1*denom2 + numer2*denom1)
+    answer.append(denom1*denom2)
+    g = math.gcd(answer[0], answer[1])
+    answer[0] = answer[0]/g
+    answer[1] = answer[1]/g
     
     return answer
