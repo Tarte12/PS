1f693c5a3cd097d461675a2363761b8ea2fd5f05
KimHuiSu <101166683+Tarte12@users.noreply.github.com>
2026-07-11 16:49:14 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.98 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,12 +1,7 @@
-from collections import deque
-
 def solution(arr):
-    stack = deque()
-    
-    for i in arr:
-        # if stack: 스택이 비어있지 않을 때
-        # stack[-1]: 스택의 마지막 원소
-        if not stack or stack[-1] != i:
-            stack.append(i)
-        
-    return list(stack)
+    answer = []
+    answer.append(arr[0])
+    for x in arr:
+        if answer[-1] != x:
+            answer.append(x)
+    return answer
