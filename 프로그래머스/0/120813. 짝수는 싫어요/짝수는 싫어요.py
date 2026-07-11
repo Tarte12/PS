f87bdbfcc1050082f3206ef86739410384ddd816
KimHuiSu <101166683+Tarte12@users.noreply.github.com>
2026-07-11 15:06:45 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,3 @@
 def solution(n):
-    answer = []
-    for i in range(1, n+1):
-        if i % 2 == 1:
-            answer.append(i)
-    return answer
+    res = [x for x in range(1, n+1, 2)]
+    return res
