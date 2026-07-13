6098e8558f5a2f4bb9a4410b29eecd909ff42fb8
KimHuiSu <101166683+Tarte12@users.noreply.github.com>
2026-07-13 11:37:32 +0900

    [level 0] Title: A로 B 만들기, Time: 0.20 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,10 +1,4 @@
 def solution(before, after):
-    
-    lst_before = list(before)
-    lst_after = list(after)
-    for i in lst_before:
-        if i in lst_after:
-            lst_after.remove(i)
-        else:
-            return 0
-    return 1
+    before = list(before)
+    after = list(after)
+    return 1 if sorted(before) == sorted(after) else 0
